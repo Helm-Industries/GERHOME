d76e2a22ba52e6ffe8948c6f6a40d523d0eb0783
nextrie <40547005+nextrie@users.noreply.github.com>
2018-11-02 17:47:57 +0100

    QuickGameFinder

---
+++ QGF/Client/QGF/QGF/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+
+namespace QGF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string ip = "192.168.0.1";
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 5000;
+            TcpClient client = new TcpClient();
+            client.Connect(ip, port);
+            NetworkStream ns = client.GetStream();
+            Thread thread = new Thread(o => ReceiveData((TcpClient)o));
+
+            thread.Start(client);
+
+            string s = "AuthRequest|nextriepvp|alexandre";
+
+            byte[] buffer = Encoding.ASCII.GetBytes(s);
+            ns.Write(buffer, 0, buffer.Length);
+
+        }
+
+        static void ReceiveData(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            byte[] receivedBytes = new byte[1024];
+            int byte_count;
+
+            while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+            {
+                MessageBox.Show(Encoding.ASCII.GetString(receivedBytes, 0, byte_count));
+            }
+        }
+
+    }
+}
---
+++ QGF/Client/QGF/QGF/Form2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QGF
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        int opacity = 0;
+        bool inout = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (inout == false)
+            {
+                FadeIn();
+            }
+            if(inout == true)
+            {
+                FadeOut();
+            }
+        }
+        int fadingSpeed = 4;
+
+        public void FadeIn()
+        {
+            label1.ForeColor = Color.FromArgb(label1.ForeColor.R + fadingSpeed, label1.ForeColor.G + fadingSpeed, label1.ForeColor.B + fadingSpeed);
+            label2.ForeColor = Color.FromArgb(label1.ForeColor.R + fadingSpeed, label1.ForeColor.G + fadingSpeed, label1.ForeColor.B + fadingSpeed);
+            if (label1.ForeColor.R >= this.BackColor.R && inout == false)
+            {
+
+                label1.ForeColor = this.BackColor;
+                label2.ForeColor = this.BackColor;
+                inout = true;
+            }
+        }
+        public void FadeOut()
+        {
+           try
+            {
+                label1.ForeColor = Color.FromArgb(label1.ForeColor.R - fadingSpeed, label1.ForeColor.G - fadingSpeed, label1.ForeColor.B - fadingSpeed);
+                label2.ForeColor = Color.FromArgb(label1.ForeColor.R - fadingSpeed, label1.ForeColor.G - fadingSpeed, label1.ForeColor.B - fadingSpeed);
+            }
+            catch
+            {
+                //label1.ForeColor = this.BackColor;
+                //label2.ForeColor = this.BackColor;
+
+                inout = false;
+            }
+            if (label1.ForeColor.R >= this.BackColor.R && inout == false)
+            {
+                label1.ForeColor = this.BackColor;
+                label2.ForeColor = this.BackColor;
+                FadeIn();
+                inout = false;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
